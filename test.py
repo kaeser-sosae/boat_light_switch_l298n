40f28f0c982f3cdda3af28fb19990642b070a4b1
Gavin Kennedy <gavin@albanylan.com>
2021-08-08 22:01:39 +1000

    Testing...

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ def setup():#Motor initialization
     try:
         #pwm_A = GPIO.PWM(Motor_A_EN, 10000)
         #pwm_A = HardwarePWM(0, hz=100)
-        pi.set_PWM_frequency(18,1)
+        pi.set_PWM_frequency(18,100000)
     except:
         print('Could not define PWM pin')
 
